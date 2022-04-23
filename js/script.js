f3afc425c17a6f520d2afcff70cff1930f845ece
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2022-04-23 21:22:40 +0300

    Change style form and add information

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 jQuery(document).ready(function () {
      
-    $(".phone").mask("+375 (99) 999-99-99"); 
-    
+    $(".phone").mask("+375 (99) 999-99-99");    
    
     jQuery('.btn').click( function() {
     	var form = jQuery(this).closest('form');
     	
     	if ( form.valid() ) {
-    		form.css('opacity','.5');
     		var actUrl = form.attr('action');
 
     		jQuery.ajax({
@@ -16,9 +14,7 @@ jQuery(document).ready(function () {
     			dataType: 'html',
     			data: form.serialize(),
     			success: function(data) {
-    				form.html(data);
-    				form.css('opacity','1');
-                    // form.find('.status').html('Форма отправлена успешно');
+                    form.find('.status').html('Форма отправлена успешно');
     			},
     			error:	 function() {
     			     form.find('.status').html('Серверная ошибка');
